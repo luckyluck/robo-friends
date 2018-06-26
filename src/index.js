80716b13873221f4db3c6dec909d6b8c911eeeb5
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-06-26 19:33:06 +0600

    Clean up applications. Add Card component with some prepared data.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'tachyons';
+
+import { robots } from './robots';
+
 import './index.css';
-import App from './App';
+import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div>
+    {robots.map(robot => <Card key={robot.id} id={robot.id} name={robot.name} email={robot.email} />)}
+</div>, document.getElementById('root'));
 registerServiceWorker();

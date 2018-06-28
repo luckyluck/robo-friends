b2e624d6f86ea4702450c65366e24ee152cde7c0
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-06-28 20:32:41 +0600

    Replace static data with placeholder request.

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,16 +3,28 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 
-import { robots } from '../robots';
-
 class App extends Component {
     constructor() {
         super();
 
         this.state = {
-            robots,
-            filteredRobotList: robots
+            robots: [],
+            filteredRobotList: []
         };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => {
+                this.setState({
+                    robots: users,
+                    filteredRobotList: users
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
     onSearchChange = event => {
@@ -27,13 +39,19 @@ class App extends Component {
     };
 
     render() {
-        return (
-            <div className="tc">
-                <h1 className="f1">RoboFriends</h1>
-                <SearchBox change={this.onSearchChange} />
-                <CardList robots={this.state.filteredRobotList}/>
-            </div>
-        );
+        if (this.state.robots.length === 0) {
+            return (
+                <h1 className="f1 tc">Loading...</h1>
+            );
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox change={this.onSearchChange}/>
+                    <CardList robots={this.state.filteredRobotList}/>
+                </div>
+            );
+        }
     }
 }
 

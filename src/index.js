4f814a8610c309b08e5c493d4e007c8d453a7fe6
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-06-29 13:56:05 +0600

    Create store and pass it down to application.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import { searchRobots } from './store/searchRobots';
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducers = combineReducers({
+    searchRobots
+});
+const store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();

f3f2242c848c16fa7f8d4542c17ae745dfdafe25
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-06-29 15:28:23 +0600

    Add logger middleware for redux to monitor our store.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import { Provider } from 'react-redux';
 
 import { searchRobots } from './store/searchRobots';
@@ -13,7 +14,7 @@ import App from './containers/App';
 const reducers = combineReducers({
     searchRobots
 });
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(createLogger()));
 
 ReactDOM.render(
     <Provider store={store}>

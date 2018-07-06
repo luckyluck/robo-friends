08a57c4dddd8d67d1d7f98cce3118426f60fd390
Alexandr Ketov <ketov.alex.v@gmail.com>
2018-07-06 18:23:36 +0600

    Add first snapshot test.

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import Card from './Card';
 
 describe('Card component', () => {
+    it('should shallowly render component', () => {
+        expect(shallow(<Card/>)).toMatchSnapshot();
+    });
+
     it('should shallowly render 1 component', () => {
         expect(shallow(<Card/>).length).toEqual(1);
     });
